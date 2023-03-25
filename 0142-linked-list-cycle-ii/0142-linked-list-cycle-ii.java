39e9934e1ac19dcd5493c6382a2ab2a4b5e6d3cb
Priti Patil <pritipatil25727@gmail.com>
2023-03-26 00:25:50 +0530

    Time: 0 ms (100.00%), Space: 42.8 MB (65.65%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
@@ -10,53 +10,23 @@
  * }
  */
 public class Solution {
-    public int lengthCycle(ListNode head) {
-        ListNode fast = head;
-        ListNode slow = head;
-
-        while (fast != null && fast.next != null) {
-            fast = fast.next.next;
-            slow = slow.next;
-            if (fast == slow) {
-                // calculate the length
-                ListNode temp = slow;
-                int length = 0;
-                do {
-                    temp = temp.next;
-                    length++;
-                } while (temp != slow);
-                return length;
-            }
-        }
-        return 0;
-    }
     public ListNode detectCycle(ListNode head) {
-        ListNode fast = head;
-        ListNode slow = head;
-        int length = 0;
-        while (fast != null && fast.next != null) {
-            fast = fast.next.next;
+        ListNode slow = head, fast = head;
+        while(fast != null && fast.next != null){
             slow = slow.next;
-            if (fast == slow) {
-                //Step 1 - Detect length of cyle
-                length = lengthCycle(slow);
-                break;
-            }
+            fast = fast.next.next;
+            if(slow == fast) break;
         }
-        if(length == 0){
+        //No cycle detected return null
+        if(fast == null || fast.next == null){
             return null;
         }
-        //Step 2 - Move second length times from head
-        ListNode first = head, second = head;
-        while(length > 0){
-            second = second.next;
-            length--;
+        ListNode temp = head;
+        while(temp != slow){
+            temp = temp.next;
+            slow = slow.next;
         }
-        //Step 3 - Move first & second k=l-(l-k) times
-        while(first != second){
-            first = first.next;
-            second = second.next;
-        }
-        return first;
+        //both slow and temp pointing to same node so return anyone
+        return temp;
     }
 }
