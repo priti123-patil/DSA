b2acc339c33b0a0f17021ad45c1c2485cc023c1f
Priti Patil <pritipatil25727@gmail.com>
2023-03-29 20:55:15 +0530

    Time: 10 ms (48.57%), Space: 43 MB (51.46%) - LeetHub

---
+++ 0707-design-linked-list/0707-design-linked-list.java
@@ -0,0 +1,65 @@
+class MyLinkedList {
+    int length = 0;
+    ListNode dummy = new ListNode(0);
+    public MyLinkedList() {
+        
+    }
+    
+    public int get(int index) {
+        if (index < 0 || index >= length)
+            return -1;
+        ListNode curr = dummy.next;
+        for(int i = 0; i < index; ++i)
+            curr = curr.next;
+        return curr.val;
+    }
+    
+    public void addAtHead(int val) {
+        ListNode head = dummy.next;
+        ListNode node = new ListNode(val);
+        node.next = head;
+        dummy.next = node;
+        ++length;
+    }
+    
+    public void addAtTail(int val) {
+        ListNode curr = dummy;
+        while (curr.next != null)
+            curr = curr.next;
+        curr.next = new ListNode(val);
+        ++length;
+    }
+    
+    public void addAtIndex(int index, int val) {
+        if (index > length)
+            return;
+        ListNode curr = dummy;
+        for(int i = 0; i < index; ++i)
+            curr = curr.next;
+        ListNode cache = curr.next;
+        ListNode node = new ListNode(val);
+        node.next = cache;
+        curr.next = node;
+        ++length;
+    }
+    public void deleteAtIndex(int index) {
+        if (index < 0 || index >= length)
+            return;
+        ListNode curr = dummy;
+        for(int i = 0; i < index; ++i)
+            curr = curr.next;
+        ListNode cache = curr.next;
+        curr.next = cache.next;
+        --length;
+    }
+}
+
+/**
+ * Your MyLinkedList object will be instantiated and called as such:
+ * MyLinkedList obj = new MyLinkedList();
+ * int param_1 = obj.get(index);
+ * obj.addAtHead(val);
+ * obj.addAtTail(val);
+ * obj.addAtIndex(index,val);
+ * obj.deleteAtIndex(index);
+ */
