aeb245af9011fb55a528bd85b8cb95674438dc55
Priti Patil <pritipatil25727@gmail.com>
2023-06-16 22:49:37 +0530

    Time: 4 ms (88.53%), Space: 45.5 MB (22.89%) - LeetHub

---
+++ 0077-combinations/0077-combinations.java
@@ -0,0 +1,18 @@
+class Solution {
+    public List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> ans = new ArrayList<List<Integer>>();
+        if (k > n || k < 0) {
+            return ans;
+        }
+        if (k == 0) {
+            ans.add(new ArrayList<Integer>());
+            return ans;
+        }
+        ans = combine(n - 1, k - 1);
+        for (List<Integer> list : ans) {
+            list.add(n);
+        }
+        ans.addAll(combine(n - 1, k));
+        return ans;
+    }
+}
