4be78e56497e3e21e843d0dc4243eb4624e41bf8
Priti Patil <pritipatil25727@gmail.com>
2023-09-01 22:16:34 +0530

    Time: 2 ms (80.97%), Space: 40.3 MB (75.26%) - LeetHub

---
+++ 0390-elimination-game/0390-elimination-game.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int lastRemaining(int n) {
+        int head = 1, step = 1;
+        boolean dir = true; 
+        while (n > 1) {
+            if (dir)
+                head += step;
+            else
+                head += n%2 == 0? 0 : step;
+            step *= 2;
+            n /= 2;
+            dir =! dir;
+        }
+        return head;
+    }
+}
